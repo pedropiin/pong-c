dbf9b18a9e839e216c075651c0310ec58a00145b
Pedro da Rosa Pinheiro <p231081@dac.unicamp.br>
2023-01-11 01:32:40 -0300

    mecanicas da barra completas

--- pong.c
+++ pong.c
@@ -47,9 +47,22 @@ struct termios transicao_canonico() {
     return oldt;
 }
 
-int atualiza_barras(int dir_esquerda, int dir_direita, int *ybarra_esquerda, int *ybarra_direita) {
-    *ybarra_esquerda = (*ybarra_esquerda + dir_esquerda + ROWS) % ROWS;
-    *ybarra_direita = (*ybarra_direita + dir_direita + ROWS) % ROWS;
+void atualiza_barras(int dir_esquerda, int dir_direita, int *ybarra_esquerda, int *ybarra_direita) {
+    if (*ybarra_esquerda <= 13 && *ybarra_esquerda >= 1) {
+        *ybarra_esquerda = (*ybarra_esquerda + dir_esquerda + ROWS) % ROWS;
+    } else if (*ybarra_esquerda >= 13 && dir_esquerda != 1) {
+        *ybarra_esquerda = (*ybarra_esquerda + dir_esquerda + ROWS) % ROWS;
+    } else if (*ybarra_esquerda <= 1 && dir_esquerda != -1) {
+        *ybarra_esquerda = (*ybarra_esquerda + dir_esquerda + ROWS) % ROWS;
+    }
+
+    if (*ybarra_direita <= 13 && *ybarra_direita >= 1) {
+        *ybarra_direita = (*ybarra_direita + dir_direita + ROWS) % ROWS;
+    } else if (*ybarra_direita >= 13 && dir_direita != 1) {
+        *ybarra_direita = (*ybarra_direita + dir_direita + ROWS) % ROWS;
+    } else if (*ybarra_direita <= 1 && dir_direita != -1) {
+        *ybarra_direita = (*ybarra_direita + dir_direita + ROWS) % ROWS;
+    }
 }
 
 int checa_fim_jogo(int pontos_esquerda, int pontos_direita) {
@@ -60,15 +73,18 @@ int checa_fim_jogo(int pontos_esquerda, int pontos_direita) {
 }
 
 void desenha_barras(int ybarra_esquerda, int ybarra_direita) {
-    for (int i = 0; i <= TAMANHO_BARRA; i++) {
+    for (int i = 1; i <= TAMANHO_BARRA + 1; i++) {
         printf("\e[%iB\e[%iC▓", ybarra_esquerda + i, COL_BARRA_ESQUERDA + 1);
         printf("\e[%iF", ybarra_esquerda + i);
+    }
+    for (int i = 0; i <= TAMANHO_BARRA; i++) {
         printf("\e[%iB\e[%iC▓", ybarra_direita + i, COL_BARRA_DIREITA + 1);
         printf("\e[%iF", ybarra_direita + i);
     }
+    fflush(stdout);
 }
 
-void le_comandos(int *dir_esquerda, int *dir_direita, int *quit) {
+void le_comandos(int *dir_esquerda, int *dir_direita, int *quit, int ybarra_esquerda, int ybarra_direita) {
     struct timeval tv;
     fd_set fds;
     tv.tv_sec = 0;
@@ -78,23 +94,17 @@ void le_comandos(int *dir_esquerda, int *dir_direita, int *quit) {
     FD_SET(STDIN_FILENO, &fds);
     select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
     if (FD_ISSET(STDIN_FILENO, &fds)) {
-        switch(getch()) {
-            case 'w':
-                *dir_esquerda = -1;
-                break;
-            case 'a':
-                *dir_esquerda = 1;
-                break;
-            case '\033':
-                getch();
-                switch(getch()) {
-                    case 'A':
-                        *dir_direita = -1;
-                        break;
-                    case 'B':
-                        *dir_direita = 1;
-                        break;
-                }
+        int tecla = getchar();
+        if (tecla == 'q') {
+            *quit = 1;
+        } else if (tecla == 'w') {
+            *dir_esquerda = -1;
+        } else if (tecla == 's') {
+            *dir_esquerda = 1;
+        } else if (tecla == 'o') {
+            *dir_direita = -1;
+        } else if (tecla == 'l') {
+            *dir_direita = 1;
         }
     }
 }
@@ -109,24 +119,23 @@ int main() {
     int quit = 0, fim_jogo = 0, pontos_esquerda = 0, pontos_direita = 0;
     int dir_esquerda = 0, dir_direita = 0;
 
-    while (!quit || !fim_jogo) {
+    while (!quit && !fim_jogo) {
         printa_tabuleiro();
         printf("\e[%iA", ROWS + 2);
 
         atualiza_barras(dir_esquerda, dir_direita, &ybarra_esquerda, &ybarra_direita);
-        desenha_barras(ybarra_esquerda, ybarra_direita);
         fim_jogo = checa_fim_jogo(pontos_esquerda, pontos_direita);
-        
+        desenha_barras(ybarra_esquerda, ybarra_direita);
+
         usleep(5 * 1000000 / 60);
 
-        le_comandos(&dir_esquerda, &dir_direita, &quit);
+        le_comandos(&dir_esquerda, &dir_direita, &quit, ybarra_esquerda, ybarra_direita);
 
-        break;
     }
 
 
-    printf("\e[%iB", ROWS + 2);
     printf("\e[?25h"); //reaparecer cursor
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    system("clear");
     return 0;
 }
